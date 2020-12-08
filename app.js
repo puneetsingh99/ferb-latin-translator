4470019e66ccedbcb7ce92d3d85e7b521dffe9bf
puneetsingh99 <puneetsingh@github.com>
2020-12-08 08:15:13 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+var translate = document.querySelector("#btn-translate");
+
+var output = document.querySelector("#txt-output");
+
+function errorHandler(error){
+    console.log("error: ", error);
+    console.log("Something went wrong, please try again after sometime");
+}
+
+
+
+function translator(){
+    var input = document.querySelector("#txt-input");
+    var inputText = input.value;
+    var serverUrl = "https://api.funtranslations.com/translate/ferb-latin.json"
+    const url = serverUrl + "?"+"text="+inputText; 
+    fetch(url)
+    .then(response => response.json())
+    .then(json => output.innerText = json.contents.translated)
+    .catch(errorHandler)
+    
+}
+
+translate.addEventListener("click", translator);
